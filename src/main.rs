d13265889c3f89b4df92774d3d9bc85183a7f362
Kaede Fujisaki <kaede@hexe.net>
2021-06-07 01:34:18 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use bevy::prelude::*;
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
+enum MyStage {
+  BeforeRound,
+  AfterRound,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum MyState {
+  UpdateState,
+}
+
+fn print_before_system() {
+  println!("Before Update");
+}
+
+fn print_after_system() {
+  println!("After Update");
+}
+fn print_system() {
+  println!("Update");
+}
+
+fn main() {
+  let before_system_set = SystemSet::on_update(MyState::UpdateState)
+    .with_system(print_before_system.system());
+  let after_system_set = SystemSet::on_update(MyState::UpdateState)
+    .with_system(print_after_system.system());
+  let update_system_set = SystemSet::on_update(MyState::UpdateState)
+    .with_system(print_system.system());
+  App::build()
+    .add_plugins(DefaultPlugins)
+    .add_state_to_stage(CoreStage::Update, MyState::UpdateState)
+    .add_stage_before(
+      CoreStage::Update,
+      MyStage::BeforeRound,
+      SystemStage::parallel(),
+    )
+    .add_stage_after(
+      CoreStage::Update,
+      MyStage::AfterRound,
+      SystemStage::parallel(),
+    )
+    .add_system_set_to_stage(MyStage::BeforeRound, before_system_set)
+    .add_system_set_to_stage(CoreStage::Update, update_system_set)
+    .add_system_set_to_stage(MyStage::AfterRound, after_system_set)
+    .run();
+}
